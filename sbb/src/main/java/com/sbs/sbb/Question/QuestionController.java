81bf45f7b15b57c8cf3e2777d5d928f9d801ad7b
ajunsu5@gmail.com <ajunsu5@gmail.com>
2024-05-09 17:23:14 +0900

    form2

--- sbb/src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ sbb/src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -1,8 +1,11 @@
 package com.sbs.sbb.Question;
 
+import com.sbs.sbb.Answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +27,7 @@ public class QuestionController {
 
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question q = this.questionService.getQuestion(id);
         model.addAttribute("question", q);
         return "question_detail";
@@ -35,5 +38,18 @@ public class QuestionController {
     @ResponseBody
     public String temp() {
         return "전송완료";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
